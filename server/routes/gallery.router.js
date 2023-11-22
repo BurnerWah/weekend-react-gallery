bb00c3e694ce9a832c02b1912bbced7f78f312fd
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-11-22 16:05:46 -0600

    Formatting

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express')
+const router = Router()
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   // code here
-});
+})
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
-});
+})
 
-module.exports = router;
+module.exports = router
