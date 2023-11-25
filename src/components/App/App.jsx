c704b052f902c825696bf76293f7334701eb02c8
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-11-25 08:41:28 -0600

    Added form for client to add images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,36 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import GalleryForm from '../GalleryForm/GalleryForm'
 import GalleryList from '../GalleryList/GalleryList'
 
 function App() {
+  const [galleryItems, setGalleryItems] = useState(
+    /** @type {GalleryDBItem[]} */ ([]),
+  )
+  const getGalleryItems = async () => {
+    try {
+      const res = await axios.get('/gallery')
+      setGalleryItems(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getGalleryItems()
+  }, [galleryItems])
+
   return (
     <div data-testid="app">
       <header>
         <h1>React Gallery</h1>
       </header>
       <main>
-        <GalleryList />
+        <GalleryForm getGalleryItems={getGalleryItems} />
+        <GalleryList
+          galleryItems={galleryItems}
+          getGalleryItems={getGalleryItems}
+        />
       </main>
     </div>
   )
